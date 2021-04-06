e2401756c23635a116eacb50a64edd18582736e2
isaacj32 <isaacj32@hotmail.com>
2021-04-06 00:25:35 -0500

    Vista interfaz paciente 1.0

---
+++ lib/XDInterfazpaciente.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'package:adobe_xd/pinned.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'dart:async';
+import 'dart:math';
+
+class XDInterfazpaciente extends StatelessWidget {
+  XDInterfazpaciente({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Stack(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(-14.0, -22.7),
+            child:
+                // Adobe XD layer: 'Fondo' (group)
+                SizedBox(
+              width: 1382.0,
+              height: 940.0,
+              child: Stack(
+                children: <Widget>[
+                  Pinned.fromSize(
+                    bounds: Rect.fromLTWH(0.0, 0.0, 1382.0, 939.5),
+                    size: Size(1382.0, 939.5),
+                    pinLeft: true,
+                    pinRight: true,
+                    pinTop: true,
+                    pinBottom: true,
+                    child:
+                        // Adobe XD layer: 'woman-1245817_1920' (shape)
+                        Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: const AssetImage('assets/Fondo.png'),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Pinned.fromSize(
+                    bounds: Rect.fromLTWH(105.5, 60.8, 230.0, 48.0),
+                    size: Size(1382.0, 939.5),
+                    pinLeft: true,
+                    pinTop: true,
+                    fixedWidth: true,
+                    fixedHeight: true,
+                    child: Text(
+                      'MEDIMINDER',
+                      style: TextStyle(
+                        fontFamily: 'Neo Sans',
+                        fontSize: 40,
+                        color: const Color(0xff000000),
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(92.2, 573.0),
+            child:
+                // Adobe XD layer: 'Completar registro' (group)
+                SizedBox(
+              width: 228.0,
+              height: 40.0,
+              child: Stack(
+                children: <Widget>[
+                  Pinned.fromSize(
+                    bounds: Rect.fromLTWH(0.0, 0.0, 227.5, 40.3),
+                    size: Size(227.5, 40.3),
+                    pinLeft: true,
+                    pinRight: true,
+                    pinTop: true,
+                    pinBottom: true,
+                    child: SvgPicture.string(
+                      _svg_gl3ib1,
+                      allowDrawingOutsideViewBox: true,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Pinned.fromSize(
+                    bounds: Rect.fromLTWH(9.0, 8.0, 209.0, 25.0),
+                    size: Size(227.5, 40.3),
+                    pinLeft: true,
+                    pinRight: true,
+                    fixedHeight: true,
+                    child: Text(
+                      'Ver horario completo',
+                      style: TextStyle(
+                        fontFamily: 'Sitka Display',
+                        fontSize: 25,
+                        color: const Color(0xffff0000),
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+class ClockView extends StatefulWidget {
+  @override
+  _ClockViewState createState() => _ClockViewState();
+}
+
+class _ClockViewState extends State<ClockView> {
+  @override
+  void initState() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 300,
+      child: Transform.rotate(
+        angle: -pi / 2,
+        child: CustomPaint(
+          painter: ClockPainter(),
+        ),
+      ),
+    );
+  }
+}
+
+class ClockPainter extends CustomPainter {
+  var dateTime = DateTime.now();
+
+  //60 sec - 360, 1 sec - 6degree
+  //12 hours  - 360, 1 hour - 30degrees, 1 min - 0.5degrees
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var centerX = size.width / 2;
+    var centerY = size.height / 2;
+    var center = Offset(centerX, centerY);
+    var radius = min(centerX, centerY);
+
+    var fillBrush = Paint()..color = Color(0xFF444974);
+
+    var outlineBrush = Paint()
+      ..color = Color(0xFFEAECFF)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 16;
+
+    var centerFillBrush = Paint()..color = Color(0xFFEAECFF);
+
+    var secHandBrush = Paint()
+      ..color = Colors.orange[300]
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5;
+
+    var minHandBrush = Paint()
+      ..shader = RadialGradient(colors: [Color(0xFF748EF6), Color(0xFF77DDFF)])
+          .createShader(Rect.fromCircle(center: center, radius: radius))
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 8;
+
+    var hourHandBrush = Paint()
+      ..shader = RadialGradient(colors: [Color(0xFFEA74AB), Color(0xFFC279FB)])
+          .createShader(Rect.fromCircle(center: center, radius: radius))
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 12;
+
+    var dashBrush = Paint()
+      ..color = Color(0xFFEAECFF)
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 1;
+
+    canvas.drawCircle(center, radius - 40, fillBrush);
+    canvas.drawCircle(center, radius - 40, outlineBrush);
+
+    var hourHandX = centerX +
+        60 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    var hourHandY = centerX +
+        60 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHandBrush);
+
+    var minHandX = centerX + 80 * cos(dateTime.minute * 6 * pi / 180);
+    var minHandY = centerX + 80 * sin(dateTime.minute * 6 * pi / 180);
+    canvas.drawLine(center, Offset(minHandX, minHandY), minHandBrush);
+
+    var secHandX = centerX + 80 * cos(dateTime.second * 6 * pi / 180);
+    var secHandY = centerX + 80 * sin(dateTime.second * 6 * pi / 180);
+    canvas.drawLine(center, Offset(secHandX, secHandY), secHandBrush);
+
+    canvas.drawCircle(center, 16, centerFillBrush);
+
+    var outerCircleRadius = radius;
+    var innerCircleRadius = radius - 14;
+    for (double i = 0; i < 360; i += 12) {
+      var x1 = centerX + outerCircleRadius * cos(i * pi / 180);
+      var y1 = centerX + outerCircleRadius * sin(i * pi / 180);
+
+      var x2 = centerX + innerCircleRadius * cos(i * pi / 180);
+      var y2 = centerX + innerCircleRadius * sin(i * pi / 180);
+      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), dashBrush);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+const String _svg_gl3ib1 =
+    '<svg viewBox="108.0 552.0 227.5 40.3" ><path transform="translate(108.0, 552.0)" d="M 0 0 L 227.5293731689453 0 L 227.5293731689453 40.29409790039063 L 0 40.29409790039063 L 0 0 Z" fill="#d1cacc" stroke="#707070" stroke-width="1" stroke-linecap="round" stroke-linejoin="bevel" /></svg>';
