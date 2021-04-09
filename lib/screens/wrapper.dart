c8a3caa0b10e6ee51a328ec9cafe2807aea063ab
Dlondono <david020897@gmail.com>
2021-04-08 21:20:24 -0500

    diferenciacion usuario paciente

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,8 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mediminder/Registropaciente2.0.dart';
+import 'package:mediminder/XDRegistropaciente.dart';
 import 'package:mediminder/models/userLocal.dart';
 import 'package:mediminder/screens/authenticate/authenticate.dart';
+import 'package:mediminder/screens/authenticate/sign_in.dart';
 import 'package:mediminder/screens/home/home.dart';
 import 'package:mediminder/services/database.dart';
 import 'package:provider/provider.dart';
@@ -11,22 +15,34 @@ import 'home/inicio.dart';
 class Wrapper extends StatelessWidget {
   String tipo;
   @override
+  UserLocal userGlob;
   Widget build(BuildContext context) {
     //user para provider
     final user=Provider.of<UserLocal>(context);
+    userGlob=user;
 
     // return either Home or Authentic widget
     if(user==null) {
       return Authenticate();
     }else{
+      buscarTipo();
+      if(userGlob.uid!=null) {
+        print("inicio");
+        print(userGlob.tipo);
+        print(userGlob.uid);
+        print("fin");
+      }
+
       print(user.uid);
-      FirebaseFirestore.instance.collection("Usuarios").doc(user.uid).get().then((value) => {verificar(value["tipo"])});
       if(tipo=="Supervisor"){
-        print(tipo);
-        return Home();
+        print(user);
+        return XDRegistropaciente();
       }
       else if(tipo=="Paciente"){
-        print(tipo);
+        print("paciente");
+        return Registropaciente();
+      }else{
+        print("hpat");
         return Home();
       }
     }
@@ -35,5 +51,10 @@ class Wrapper extends StatelessWidget {
   void verificar(String t){
     this.tipo = t;
     return;
+  }
+  void buscarTipo() async{
+    await FirebaseFirestore.instance.collection(
+        "Usuarios").doc(userGlob.uid).get().then((value) => {verificar(value["tipo"])});
+    print("tardeee");
   }
 }
